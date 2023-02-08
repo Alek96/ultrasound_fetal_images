451e19028a924b3a36f413916331ac6449875d38
Aleksander Zamojski <zamojski96@gmail.com>
2023-02-08 22:23:48 +0100

    Fix test_sweeps

--- tests/test_sweeps.py
+++ tests/test_sweeps.py
@@ -92,8 +92,8 @@ def test_optuna_sweep_ddp_sim_wandb(tmp_path):
         "+trainer.limit_train_batches=1",
         "+trainer.limit_val_batches=1",
         "+trainer.limit_test_batches=1",
-        "+datamodule.sample=true",
-        "datamodule.batch_size=10",
+        "+data.sample=true",
+        "data.batch_size=10",
         "logger=wandb",
     ]
     run_sh_command(command)
