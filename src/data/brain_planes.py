c6e76561b2b7390d32cf8249f85d8435862a6a69
Aleksander Zamojski <zamojski96@gmail.com>
2025-10-09 16:46:11 +0200

    test 1

--- src/data/brain_planes.py
+++ src/data/brain_planes.py
@@ -149,7 +149,6 @@ class BrainPlanesDataModule(LightningDataModule):
                 data_dir=self.hparams.data_dir,
                 data_name=self.hparams.data_name,
                 subset="train",
-                identified=True,
                 transform=self.train_transforms,
                 target_transform=self.target_transform,
             )
@@ -157,7 +156,6 @@ class BrainPlanesDataModule(LightningDataModule):
                 data_dir=self.hparams.data_dir,
                 data_name=self.hparams.data_name,
                 subset="val",
-                identified=True,
                 transform=self.train_transforms,
                 target_transform=self.target_transform,
             )
@@ -165,7 +163,6 @@ class BrainPlanesDataModule(LightningDataModule):
                 data_dir=self.hparams.data_dir,
                 data_name=self.hparams.data_name,
                 subset="test",
-                identified=True,
                 transform=self.test_transforms,
                 target_transform=self.target_transform,
             )
